55049c55de9b0d90ba7f3096af9b860f5a6b25a5
Alexandr <aavinogradov@bk.ru>
2023-12-14 23:06:07 +0300

    первая программа

---
+++ first_programm.java
@@ -0,0 +1,11 @@
+public class first_programm {
+
+    public static void main(String[] args) {
+        int num_a = 5;
+        int num_b = 6;
+        System.out.println("Z");
+        System.out.println("x");
+        System.out.println(num_a + num_b);
+    }
+
+}
