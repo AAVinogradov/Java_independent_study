380735603bfcab79c596c1173c9ef2399ea1921b
Alexandr <aavinogradov@bk.ru>
2024-02-01 23:34:47 +0300

    Задача с игрой угадай чмсло выполнена через бесконечный цикл

--- MyFirstApp/4_GuessTheNumber/GuessTheNumber2.java
+++ MyFirstApp/4_GuessTheNumber/GuessTheNumber2.java
@@ -1,9 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessTheNumber2 {
     public static void main(String[] args) {
-        int a = 0;
-        while (a < 10) {
-            a++;
-            System.out.println(a);
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int aiNumber = random.nextInt(10);
+        System.out.println("Компьютер загад число от 1 до 10 включительно");
+        while (true) {
+            System.out.println("Попробуйте угадать число");
+            int answer = scanner.nextInt();
+            if (answer == aiNumber) {
+                System.out.println("Угадали!");
+                scanner.close();
+                break;
+            } else {
+                System.out.println("Не угадали");
+            }
         }
     }
 }
