2c2b2d2cc8c9055e1ec9b9e609a550de69371e18
Alexandr <aavinogradov@bk.ru>
2024-02-05 23:12:54 +0300

    РПГ игра - выполнен код с основными действиями героев

---
+++ RPG_Game/ConsoleRPG.java
@@ -0,0 +1,38 @@
+package RPG_Game;
+
+import java.util.Scanner;
+
+public class ConsoleRPG {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя героя: ");
+        String heroName = scanner.next();
+        int heroHP = 10;
+        int heroAttak = 3;
+
+        String monsterName = "Орк";
+        int monsterHP = 8;
+        int monsterAttak = 2;
+        
+        System.out.println("Игра началась!");
+        System.out.println("Герой " + heroName + " вошел в лес и видит перед собой монстра по имени " + monsterName + ". Деваться некуда, придется вступить в бой.");
+        System.out.println("Бой начинается!");
+        System.out.println("выберите действие: 1 - атаковать. 2 - защищаться.");
+        int command = scanner.nextInt();
+        if (command == 1) {
+            System.out.println("Вы атаковали монстра по имени " + monsterName + " и он получает " + heroAttak + " единиц урона");
+            monsterHP -= heroAttak;
+        } else if (command == 2) {
+            System.out.println("Вы решили защищаться от монстра по имени " + monsterName + " и получаете 1 единицу здоровья");
+            heroHP ++;
+        } else {
+            System.out.println("Не верное действие! Вы пропускаете ход...");
+        }
+
+
+        System.out.println("Игра закончилась");
+
+
+        
+    }
+}
