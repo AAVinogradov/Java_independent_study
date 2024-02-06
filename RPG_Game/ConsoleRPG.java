8c3fb1ec66e37594fb7ae72999865339055c8531
Alexandr <aavinogradov@bk.ru>
2024-02-06 19:45:17 +0300

    Добавил рандом в игру

--- RPG_Game/ConsoleRPG.java
+++ RPG_Game/ConsoleRPG.java
@@ -1,27 +1,37 @@
 package RPG_Game;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class ConsoleRPG {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         System.out.println("Введите имя героя: ");
         String heroName = scanner.next();
         int heroHP = 10;
         int heroAttak = 4;
-
         String monsterName = "Орк";
         int monsterHP = 8;
         int monsterAttak = 3;
-        
         System.out.println("Игра началась!");
-        System.out.println("Герой " + heroName + " вошел в лес и видит перед собой монстра по имени " + monsterName + ". Деваться некуда, придется вступить в бой.");
+        System.out.println("Герой  в " + heroName + " вошел в лес и видит перед собой монстра по имени " + monsterName + ". Деваться некуда, придется вступитьбой.");
         System.out.println("Бой начинается!");
+        while (true) 
+        // Логика хода игрока
         System.out.println("Ход героя: выберите действие: 1 - атаковать. 2 - защищаться.");
         int command = scanner.nextInt();
         if (command == 1) {
             System.out.println("Вы атаковали монстра по имени " + monsterName + " и он получает " + heroAttak + " единиц урона");
             monsterHP -= heroAttak;
+            if (random.nextInt(100) < 10) {
+                System.out.println("Вам удалось нанести суперудар против монстра по имени " + monsterName + " и он получает дополнительные " + heroAttak + " единиц урона");
+                monsterHP -= heroAttak;
+            }
+            if (monsterHP <= 0) {
+                System.out.println("" + heroName + " победил! У монстра по имени " + monsterName + " закончились жизни.");
+                break;
+            }
         } else if (command == 2) {
             System.out.println("Вы решили защищаться от монстра по имени " + monsterName + " и получаете 1 единицу здоровья.");
             heroHP ++;
@@ -29,12 +39,23 @@ public class ConsoleRPG {
             System.out.println("Не верное действие! Вы пропускаете ход...");
         }
         System.out.println("Промежуточная статистика: Герой " + heroName + " имеет " + heroHP + " единиц здоровья. Монстр " + monsterName + " имеет " + monsterHP + " единиц здоровья");
-
-
+        //Логика хода монстра
+        System.out.println("Ход монстра " + monsterName + ".");
+        int monsterCommand = random.nextInt(2);
+        if (monsterCommand == 0) {
+            System.out.println("Монстр " + monsterName +  " атакует, герой " + heroName + " получает " + monsterAttak + " единиц урона");
+            heroHP -= monsterAttak;
+            if (heroHP <= 0) {
+                System.out.println("" + monsterName + " победил! У героя " + heroName + " закончились жизни.");
+                break;
+            }
+        } else if (monsterCommand == 1) {
+            System.out.println("Монстр " + monsterName +  " защищается, герой  и получает 5 единиц здоровья");
+            monsterHP += 5;
+        }
+        System.out.println("Промежуточная статистика: Герой " + heroName + " имеет " + heroHP + " единиц здоровья. Монстр " + monsterName + " имеет " + monsterHP + " единиц здоровья");
+        
 
         System.out.println("Игра закончилась");
-
-
-        
     }
 }
