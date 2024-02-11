252c8404de7fa23e4e443675bdf253b133ca0bfa
AAVinogradov <aavinogradov@bk.ru>
2024-02-11 22:56:25 +0300

    Добавил решение ДЗ 5 - положительные или отрицательные числа

---
+++ MyFirstApp/1_First/HomeWork_5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class HomeWork_5 {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число a: ");
+        int a = scanner.nextInt();
+        System.out.println("Введите число b");
+        int b = scanner.nextInt();
+        System.out.println("Введите число c");
+        int c = scanner.nextInt();
+        System.out.println("Введите число d");
+        int d = scanner.nextInt();
+        boolean result = sumPositive(a ,b, c, d);
+        System.out.println(result);
+        scanner.close();
+    }
+
+    public static boolean sumPositive(int a, int b, int c, int d) {
+        int result = a + b + c + d;
+        if (result >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+}
