2d099df860f12ea5afb79a7c10fec5fa24664c5e
AAVinogradov <aavinogradov@bk.ru>
2024-02-11 23:12:57 +0300

    Добавил решение ДЗ 7 - сравнение чисел

---
+++ MyFirstApp/1_First/HpomeWork_7.java
@@ -0,0 +1,23 @@
+// Реализуйте метод boolean isABiggerThanB(int a, int b) возвращающий true если a больше b и false в противном случае
+
+import java.util.Scanner;
+
+public class HpomeWork_7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите a: ");
+        int a = scanner.nextInt();
+        System.out.println("Введите b: ");
+        int b = scanner.nextInt();
+        System.out.println(isABiggerThanB(a, b));
+        scanner.close();
+    }
+    public static boolean isABiggerThanB(int a, int b) {
+        if (a > b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+}
