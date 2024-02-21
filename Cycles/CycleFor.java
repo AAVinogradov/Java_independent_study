c18f5892899f6a550ccf8bf986e7c2e86167dcd2
AAVinogradov <aavinogradov@bk.ru>
2024-02-21 20:39:36 +0300

    добавил несколько примеров цикла For

--- Cycles/CycleFor.java
+++ Cycles/CycleFor.java
@@ -9,6 +9,9 @@ public class CycleFor {
         int x = scanner.nextInt();
         scanner.close();
         primer1(x);
+        primer2(x);
+        primer3(x);
+        primer4(x);
     }
     public static void primer1(int x) {
         for (int i = 0; i < x; i++) {
@@ -17,4 +20,29 @@ public class CycleFor {
         System.out.println();
     }
 
+
+    public static void primer2(int x) {
+        for (int i = 5; i <= x; i++) {
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+
+
+    public static void primer3(int x) {
+        for (int i = 5; i <= x; i+= 2) {
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+
+
+    public static void primer4(int x) {
+        for (int i = 100; i >= x; i-= 10) {
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+        
 }
+
