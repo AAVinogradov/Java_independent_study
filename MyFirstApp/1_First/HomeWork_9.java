04171451c2952b5b3129a48aced535a5a6fd75c3
AAVinogradov <aavinogradov@bk.ru>
2024-02-26 22:13:15 +0300

    решены домашниии задачи HomeWork_9

--- MyFirstApp/1_First/HomeWork_9.java
+++ MyFirstApp/1_First/HomeWork_9.java
@@ -35,12 +35,72 @@
 // **********
 // Попробовать сделать диагонали и потом закрасить одну часть или 2 диагонали. Толщину линий также регулировать
 
-
-
+import java.util.Scanner;
 
 public class HomeWork_9 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите высоту: ");
+        int hightSize = scanner.nextInt();
+        System.out.println("Введите ширину: ");
+        int widthSize = scanner.nextInt();
+        scanner.close();
+        cycleInCycle1(hightSize, widthSize);
+        cycleInCycle2(hightSize, widthSize);
+        cycleInCycle3(hightSize, widthSize);
+
         
+    }
+
+    public static void cycleInCycle1(int hightSize, int widthSize) {
+        
+        for (int y = 0; y < hightSize; y++) {
+            for (int x = 0; x < widthSize; x++) {
+                if (x > 1 && x < 4 && y > 1 && y < 4) {
+                    System.out.print("0");
+                } else if (x == hightSize / 2) {
+                    System.out.print("|");
+
+                } else if (y == widthSize / 2) {
+                    System.out.print("-");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+    }
+    System.out.println();
+
+    }
+    public static void cycleInCycle2 (int hightSize, int widthSize) {
+        
+        for (int y = 0; y < hightSize; y++) {
+            for (int x = 0; x < widthSize; x++) {
+                if (x == y || x == hightSize  - 1 - y) {
+                    System.out.print("0");
+                } else {
+                    System.out.print("*");
+                }
+            }
+        System.out.println();
+
+        }    
+        System.out.println();
+    }
+
+    public static void cycleInCycle3 (int hightSize, int widthSize) {
+        
+        for (int y = 0; y < hightSize; y++) {
+            for (int x = 0; x < widthSize; x++) {
+                if (x >= 0 && x < widthSize && y >= hightSize / 2 && y < hightSize) {
+                    System.out.print("0");
+                } else {
+                    System.out.print("*");
+                }
+            }
+        System.out.println();
+
+        }    
+        System.out.println();
     }
 }
