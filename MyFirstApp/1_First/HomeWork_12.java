9f1631f317bee237b87a3bf2d4d5e229f30cd14f
AAVinogradov <aavinogradov@bk.ru>
2024-02-27 21:55:06 +0300

    добавлено решение ДЗ 13 по замене четных значений на 0

--- MyFirstApp/1_First/HomeWork_12.java
+++ MyFirstApp/1_First/HomeWork_12.java
@@ -1,5 +1,31 @@
-// Реализуйте метол, принимающий в качестве аргумента двумерный целочисленный массив и возвращающий сумму его диагональных элементов
+// Реализуйте метод, принимающий в качестве аргумента двумерный целочисленный массив и возвращающий сумму его диагональных элементов
 
 public class HomeWork_12 {
-    
+    public static void main(String[] args) {
+        int [] [] arr = {
+            {1, 2, 3, 4, 5},
+            {4, 5, 6, 7, 8},
+            {7, 8, 9, 4, 2},
+            {4, 5, 6, 7, 8},
+            {7, 8, 9, 4, 2},
+        };
+        System.out.println(sumDiagonalDoubleArray(arr));
+    }
+
+    public static int sumDiagonalDoubleArray(int [][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+
+                if (j == i && j == arr.length - 1 - i) {
+                    sum += arr[i][j];
+                }
+            }
+            System.out.println();
+
+        
+        }
+        return sum;
+    }
 }
--- MyFirstApp/1_First/HomeWork_13.java
+++ MyFirstApp/1_First/HomeWork_13.java
@@ -1,6 +1,31 @@
-// Реализуйте метол, принимающий в качестве аргумента двумерный целочисленный массив и заменяющий все четные задания на 0
+// Реализуйте метол, принимающий в качестве аргумента двумерный целочисленный массив и заменяющий все четные значения на 0
 
 
 public class HomeWork_13 {
+    public static void main(String[] args) {
+        int [] [] arr = {
+            {1, 2, 3, 4, 5},
+            {4, 5, 6, 7, 8},
+            {7, 8, 9, 4, 2},
+            {4, 5, 6, 7, 8},
+            {7, 8, 9, 4, 2},
+        };
+        replacingEvenNumbers(arr); 
+    }
+
+    public static void replacingEvenNumbers(int [][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] % 2 == 0) {
+                    array[i][j] = 0;
+                } else {
+                    array[i][j] = array[i][j];
+                    
+                }
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     
+    }
 }
