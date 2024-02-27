773ee2898596160313b7190c3184f77636220535
AAVinogradov <aavinogradov@bk.ru>
2024-02-27 20:57:38 +0300

    добавлено решение ДЗ 11 по суммированию элементов двумерного массива

--- MyFirstApp/1_First/HomeWork_11.java
+++ MyFirstApp/1_First/HomeWork_11.java
@@ -1,5 +1,23 @@
-// Реализуйте метол, принимающий в качестве аргумента двумерный целочисленный массив и возвращающий сумму его элементов
+// Реализуйте метод, принимающий в качестве аргумента двумерный целочисленный массив и возвращающий сумму его элементов
 
 public class HomeWork_11 {
+    public static void main(String[] args) {
+        int [] [] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println(sumDoubleArray(arr));
     
+    }
+
+    public static int sumDoubleArray(int [][] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                sum += array[i][j];
+            }
+        }
+    return sum;
+    }
 }
