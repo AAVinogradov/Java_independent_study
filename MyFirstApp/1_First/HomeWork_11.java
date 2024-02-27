22080c687553e8d3fa12d60ff8ccc4cc6d4d316a
AAVinogradov <aavinogradov@bk.ru>
2024-02-27 21:05:49 +0300

    добавлено еще одно решение ДЗ 11 по суммированию элементов двумерного массива

--- MyFirstApp/1_First/HomeWork_11.java
+++ MyFirstApp/1_First/HomeWork_11.java
@@ -8,6 +8,13 @@ public class HomeWork_11 {
             {7, 8, 9}
         };
         System.out.println(sumDoubleArray(arr));
+
+        int [] [] arr2 = {
+            {323, 346, 231, 12},
+            {12, 323, 112, 22, 23},
+            {23, 12, 323, 112, 22, 23}
+        };
+        System.out.println(sumDoubleArray2(arr2));
     
     }
 
@@ -17,6 +24,16 @@ public class HomeWork_11 {
             for (int j = 0; j < array[i].length; j++) {
                 sum += array[i][j];
             }
+        }
+    return sum;
+    }
+
+    public static int sumDoubleArray2 (int [] [] arr2) {
+        int sum = 0;
+        for(int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                sum += arr2 [i][j];
+            }    
         }
     return sum;
     }
