4ba7cb80ea7cb3a512a2aac3ec5dab3007e8e15e
AAVinogradov <aavinogradov@bk.ru>
2024-02-27 22:28:05 +0300

    добавлено решение ДЗ 14 вывод одномерного массива по условию

--- MyFirstApp/1_First/HomeWork_14.java
+++ MyFirstApp/1_First/HomeWork_14.java
@@ -1,6 +1,32 @@
 // Реализуйте метод, который создает одномерный целочисленный массив размером N и заполняющий его последовательностью [1, 2, 3, 4, N/2, 4, 3, 2, 1] (днина = 9), если длина не четная или [1, 2, 3, 4, N/2, N/2, 4, 3, 2, 1] (днина = 10) - если длина четная.
 
+import java.util.Scanner;
 
 public class HomeWork_14 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int size = scanner.nextInt();
+        createArray(size);
+        scanner.close();
+    }
+
+
+    public static void createArray (int size) {
+    if (size % 2 == 0) {
+        for (int i = 1; i <= size / 2; i ++) {
+            System.out.print(i + " ");
+        }
+        for (int j = size / 2; j >= 1; j --) {
+            System.out.print(j + " ");
+        }   
+    } else {
+        for (int i = 1; i <= size / 2 + 1; i ++) {
+            System.out.print(i + " ");
+        }
+        for (int j = size / 2; j >= 1; j --) {
+            System.out.print(j + " ");
+        }   
+    }
+    }
 }
